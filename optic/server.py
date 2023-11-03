f65c3e35a817dcc0d5d535eadd9fb91eb5166ceb
Will Chen <willchen90@gmail.com>
2023-11-03 16:05:49 -0700

    Simple SSE (server-sent event) server using Flask

--- optic/server.py
+++ optic/server.py
@@ -1,8 +1,25 @@
-from flask import Flask
+from flask import Flask, Response
+import time
+import json
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World111!'
+def generate_data():
+    """
+    This generator yields data in a streaming fashion which will be sent to the client.
+    For demonstration purposes, it sends JSON-encoded data, which includes a message and a timestamp.
+    """
+    count = 0
+    while True:
+        time.sleep(1)  # Simulate a delay for data preparation
+        count += 1
+        data = json.dumps({'time': time.time(), 'message': f'Data number {count}'})
+        yield f"data: {data}\n\n"
 
+@app.route('/ui')
+def ui_stream():
+    """
+    This view function is the SSE endpoint. It returns a streaming response that
+    yields data from the generator function.
+    """
+    return Response(generate_data(), content_type='text/event-stream')
