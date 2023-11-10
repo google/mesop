810ca797b15a6afd5992b9c2eb566fbf62531140
Will Chen <willchen90@gmail.com>
2023-11-10 11:12:20 -0800

    Fix up setup_proto_symlink.sh after updating .bazelrc and make it more robust

--- setup_proto_symlink.sh
+++ setup_proto_symlink.sh
@@ -1,4 +1,7 @@
 # Needed for VS Code to provide Python type-checking against generated proto stubs.
-# TODO: do not hardcode architecture in "bazel-out" path.
-touch bazel-out/darwin_arm64-fastbuild/bin/protos/__init__.py && \
-ln -s $(pwd)"/bazel-out/darwin_arm64-fastbuild/bin/protos" .dev_server.venv/lib/python3.10/site-packages/protos
+bazel_bin_path=$(bazel info bazel-bin)
+# Clean-up old symlink
+rm .dev_server.venv/lib/python3.10/site-packages/protos
+
+touch "${bazel_bin_path}/protos/__init__.py" && \
+ln -s "${bazel_bin_path}/protos" .dev_server.venv/lib/python3.10/site-packages/protos
