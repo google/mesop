fda01861bde585d620fffde653358b512899383a
Will Chen <willchen90@gmail.com>
2023-11-21 10:47:49 -0800

    Fix prod_server.py: send index.html if it's not a file path request

--- optic/server/prod_server.py
+++ optic/server/prod_server.py
@@ -1,3 +1,4 @@
+import os
 from flask import send_from_directory
 from .server import app, port
 from rules_python.python.runfiles import runfiles  # type: ignore
@@ -15,11 +16,19 @@ def serve_root():
 
 @app.route("/<path:path>")
 def serve_file(path: str):
-    return send_from_directory(get_path(), path)
+    if is_file_path(path):
+        return send_from_directory(get_path(), path)
+    else:
+        return send_from_directory(get_path(), "index.html")
 
 
 def run():
     app.run(host="0.0.0.0", port=port, use_reloader=False)
+
+
+def is_file_path(path: str) -> bool:
+    _, last_segment = os.path.split(path)
+    return "." in last_segment
 
 
 if __name__ == "__main__":
