350676d149ee5aab023e388e3e555c89be69b483
Will Chen <willchen90@gmail.com>
2023-11-20 22:28:54 -0800

    Make sure we re-execute modules imported by the optic app each time

--- optic/cli/read_module.py
+++ optic/cli/read_module.py
@@ -1,14 +1,29 @@
 import importlib.util
+import sys
 from types import ModuleType
 
 
 def read_module(module_path: str) -> ModuleType:
     try:
-        spec = importlib.util.spec_from_file_location("module_name", module_path)
+        # Keep track of all the sys modules in a set
+        # so we can compare it after we load the module
+        # to see what modules were added.
+        pre_load_modules = set(sys.modules.keys())
+
+        spec = importlib.util.spec_from_file_location("main_optic_app", module_path)
         assert spec
         module = importlib.util.module_from_spec(spec)
         assert spec.loader
         spec.loader.exec_module(module)
+
+        # Get the set of modules that were added after we loaded the module
+        post_load_modules = set(sys.modules.keys()) - pre_load_modules
+        # Delete all modules that were added after we loaded the module
+        # so that when we reload the module, we re-execute any imported modules
+        # from the main app module.
+        for module_name in post_load_modules:
+            del sys.modules[module_name]
+
         return module
     except Exception as e:
         raise e
