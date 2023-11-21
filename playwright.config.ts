66b375b56eed761e2e020f446fba5e22884dafd8
Will Chen <willchen90@gmail.com>
2023-11-21 15:54:35 -0800

    Force Playwright to not run in parallel due to flakiness

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,8 +19,8 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   retries: 2,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  /* Force non-parallelism due to flakiness. */
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
