8dc312df151aea4fbf0c821c9a3edb5ce06a5319
Will Chen <willchen90@gmail.com>
2023-11-22 14:38:00 -0800

    Move setup_proto_py_modules.sh into scripts/ dir; document scripts w/ README.md

---
+++ scripts/README.md
@@ -0,0 +1,15 @@
+# Scripts
+
+All these scripts should be executed from the root directory of the Git repo.
+
+**Example shell script usage:**
+
+```shell
+scripts/${script_name}.sh
+```
+
+**Example Python script usage:**
+
+```shell
+python scripts/gen_component.py ${component_name}
+```
--- scripts/run_py_typecheck.sh
+++ scripts/run_py_typecheck.sh
@@ -4,7 +4,7 @@ bazel build //optic/cli && \
 bazel run //optic/server:dev_server.venv && \
 source .dev_server.venv/bin/activate && \
 # Setup py modules for proto dirs
-./setup_proto_py_modules.sh && \
+./scripts/setup_proto_py_modules.sh && \
 # Install pip deps
 pip install -r optic/requirements_lock.txt && \
 # Run actual typecheck
