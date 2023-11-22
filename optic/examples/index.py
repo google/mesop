18e75905a73b981cb819e770afa61b705c32f770
Will Chen <willchen90@gmail.com>
2023-11-21 22:21:42 -0800

    Display nicely server error

--- optic/examples/index.py
+++ optic/examples/index.py
@@ -1,4 +1,5 @@
 # Use this file to import all examples
 # Use import alias so Ruff doesn't complain about unused imports
+from optic.examples import error as error
 from optic.examples import simple as simple
 from optic.examples import hello_world as hello_world
--- web/src/app/app.ts
+++ web/src/app/app.ts
@@ -9,6 +9,57 @@ import { ChannelService, ChannelStatus } from "../services/channel_service";
   selector: "app",
   templateUrl: "app.html",
   styles: `
+  .error {
+    background-color: #ffe8ef;
+    padding: 18px;
+    color: #ae2626;
+  }
+
+  .exception {
+    font-size: 1.5rem;
+    font-weight: 500;
+    padding-bottom: 16px;
+  }
+
+  .exception-title {
+    font-weight: bold;
+  }
+
+  .traceback-heading {
+    font-weight: 500;
+    font-size: 1rem;
+  }
+
+  .traceback {
+    font-family: monospace;
+    font-size: 0.8rem;
+    line-height: 1.5;
+    white-space: preserve;
+    border: 1.5px solid #d8adad;
+    padding: 8px;
+    border-radius: 12px;
+    margin: 8px 0;
+  }
+
+  .traceback-segment {
+    color: #444;
+    margin-bottom: 12px;
+  }
+
+  .traceback-segment.highlight {
+    color: #000;
+    font-weight: bold;
+  }
+
+  .show-full-traceback {
+    text-decoration: underline;
+    font-size: 0.85rem;
+  }
+
+  .show-full-traceback:hover {
+    cursor: pointer;
+  }
+
   .status {
     height: 8px;
   }
@@ -20,6 +71,7 @@ import { ChannelService, ChannelStatus } from "../services/channel_service";
 export class App {
   rootComponent: pb.Component;
   error: pb.ServerError;
+  showFullTraceback: boolean = false;
 
   constructor(
     private zone: NgZone,
@@ -41,4 +93,38 @@ export class App {
   isConnectionOpen() {
     return this.channelService.getStatus() == ChannelStatus.OPEN;
   }
+
+  getTraceback(): TracebackSegment[] {
+    const originalTraceback = this.error
+      .getTraceback()
+      .slice("Traceback (most recent call last):".length)
+      .trimStart();
+    const regex = /File ".*?\.runfiles/g;
+
+    const trimmedString = originalTraceback.replace(regex, 'File "/');
+
+    const res = trimmedString
+      .split("File")
+      .map((str) => ({
+        text: str.trimEnd(),
+        type: getTypeFromPath(str),
+      }))
+      .slice(1) as TracebackSegment[];
+
+    return res;
+  }
+
+  turnOnFullTraceBack() {
+    this.showFullTraceback = true;
+  }
+}
+
+interface TracebackSegment {
+  text: string;
+  type: "highlight" | "lowlight";
+}
+
+function getTypeFromPath(str: string): "highlight" | "lowlight" {
+  // TODO: make this logic more robust.
+  return str.includes("/examples/") ? "highlight" : "lowlight";
 }
