5ada2932554d9a9141fdfff000245e01f9be4749
Will Chen <willchen90@gmail.com>
2023-11-21 23:40:28 -0800

    Fix e2e test for box

--- optic/components/box/e2e/box_test.ts
+++ optic/components/box/e2e/box_test.ts
@@ -2,7 +2,6 @@ import { test, expect } from "@playwright/test";
 
 test("test", async ({ page }) => {
   await page.goto("/components/box/e2e/box_app");
-  expect(await page.getByText("Hello, world!").textContent()).toContain(
-    "Hello, world!",
-  );
+  expect(await page.getByText("hi1").textContent()).toContain("hi1");
+  expect(await page.getByText("hi2").textContent()).toContain("hi2");
 });
