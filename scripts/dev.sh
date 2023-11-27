9ef17829730e4e45a172a2bdd367a32013722ad1
Will Chen <willchen90@gmail.com>
2023-11-26 20:20:23 -0800

    Create dev script to launch py and web dev together

---
+++ scripts/dev.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Define your commands
+cmd1="scripts/run_web_dev.sh"
+cmd2="scripts/run_py_dev.sh"
+
+# Run both commands in subshells, redirect stderr to stdout, and prefix output
+{ $cmd1 2>&1 >&3 | sed 's/^/[WEB] /' >&2; } 3>&1 &
+{ $cmd2 2>&1 >&3 | sed 's/^/[PY] /' >&2; } 3>&1 &
+
+# Wait for both background processes to finish
+wait
