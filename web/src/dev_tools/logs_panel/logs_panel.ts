712c35fa00b409d8b9f58861b74315864f76c182
Will Chen <willchen90@gmail.com>
2023-11-27 00:04:40 -0800

    Scaffold object tree component

--- web/src/dev_tools/logs_panel/logs_panel.ts
+++ web/src/dev_tools/logs_panel/logs_panel.ts
@@ -1,11 +1,13 @@
 import { Component } from "@angular/core";
 import { LoggerService } from "../../services/logger_service";
+import { ObjectTree } from "../object_tree/object_tree";
 
 @Component({
   selector: "optic-logs-panel",
   templateUrl: "logs_panel.html",
   styleUrl: "logs_panel.css",
   standalone: true,
+  imports: [ObjectTree],
 })
 export class LogsPanel {
   constructor(private loggerService: LoggerService) {
---
+++ web/src/dev_tools/object_tree/object_tree.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { LoggerService } from "../../services/logger_service";
+
+@Component({
+  selector: "optic-object-tree",
+  templateUrl: "object_tree.html",
+  styleUrl: "object_tree.css",
+  standalone: true,
+})
+export class ObjectTree {}
