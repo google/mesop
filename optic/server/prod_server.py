c7d6c1b0c52312f650d92bee6077088a4019d370
Will Chen <willchen90@gmail.com>
2023-12-08 11:42:30 -0800

    Use send_file instead of send_from_directory for better downstream compat

--- optic/server/prod_server.py
+++ optic/server/prod_server.py
@@ -1,6 +1,7 @@
 import os
 
-from flask import send_from_directory
+from flask import send_file
+from werkzeug.security import safe_join
 
 from optic.utils.runfiles import get_runfile_location
 
@@ -8,21 +9,23 @@ from .flags import port
 from .server import app
 
 
-def get_path():
-  return get_runfile_location("optic/optic/web/src/app/prod/web_package")
+def get_path(path: str):
+  safe_path = safe_join("optic/optic/web/src/app/prod/web_package", path)
+  assert safe_path
+  return get_runfile_location(safe_path)
 
 
 @app.route("/")
 def serve_root():
-  return send_from_directory(get_path(), "index.html")
+  return send_file(get_path("index.html"))
 
 
 @app.route("/<path:path>")
 def serve_file(path: str):
   if is_file_path(path):
-    return send_from_directory(get_path(), path)
+    return send_file(get_path(path))
   else:
-    return send_from_directory(get_path(), "index.html")
+    return send_file(get_path("index.html"))
 
 
 def run():
