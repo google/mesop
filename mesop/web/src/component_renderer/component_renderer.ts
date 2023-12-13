3f1e9ded4866c67ece33d20a5deb9d7680ed9e36
Will Chen <willchen90@gmail.com>
2023-12-12 23:33:13 -0800

    Remove <mesop-box> Angular component because we cannot have intermediate DOM nodes due to flexbox styling rules

--- mesop/web/src/component_renderer/component_renderer.ts
+++ mesop/web/src/component_renderer/component_renderer.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, HostBinding, Input} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {Component as ComponentProto} from 'mesop/mesop/protos/ui_jspb_proto_pb/mesop/protos/ui_pb';
 import {CheckboxComponent} from '../../../components/checkbox/checkbox';
@@ -7,8 +7,8 @@ import {TextComponent} from '../../../components/text/text';
 // REF(//scripts/gen_component.py):insert_ts_import
 import {MarkdownComponent} from '../../../components/markdown/markdown';
 import {TextInputComponent} from '../../../components/text_input/text_input';
-import {BoxComponent} from '../../../components/box/box';
 import {ComponentLoader} from './component_loader';
+import {BoxType} from 'mesop/mesop/components/box/box_jspb_proto_pb/mesop/components/box/box_pb';
 
 @Component({
   selector: 'component-renderer',
@@ -18,7 +18,6 @@ import {ComponentLoader} from './component_loader';
     // REF(//scripts/gen_component.py):insert_ng_import
     MarkdownComponent,
     TextInputComponent,
-    BoxComponent,
     TextComponent,
     CheckboxComponent,
     ButtonComponent,
@@ -28,6 +27,7 @@ import {ComponentLoader} from './component_loader';
 })
 export class ComponentRenderer {
   @Input() component!: ComponentProto;
+  private _boxType: BoxType | undefined;
 
   trackByFn(index: any, item: ComponentProto) {
     const key = item.getKey()?.getKey();
@@ -43,5 +43,24 @@ export class ComponentRenderer {
 
   key() {
     return this.component.getKey()!;
+  }
+
+  ngOnChanges() {
+    if (this.component.getType()?.getName() !== 'box') {
+      return;
+    }
+    this._boxType = BoxType.deserializeBinary(
+      this.component.getType()!.getValue() as unknown as Uint8Array,
+    );
+  }
+
+  @HostBinding('style') get style(): string {
+    if (!this._boxType) {
+      return '';
+    }
+
+    return `
+       display: block;
+       background-color: ${this._boxType.getBackgroundColor()};`;
   }
 }
