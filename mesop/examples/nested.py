53b4a9ff0bf47ac176ccecf0f04a0de74ce84ee1
Will Chen <willchen90@gmail.com>
2023-12-13 20:13:41 -0800

    Fix python type-checking errors in nested.py

--- mesop/examples/nested.py
+++ mesop/examples/nested.py
@@ -16,10 +16,10 @@ def click(event: me.ClickEvent):
 def app():
   state = me.state(State)
 
-  with me.box(background_color="pink"):
+  with me.box(styles="background-color: pink"):
     me.text(text="hi1")
-    with me.box(background_color="blue"):
+    with me.box(styles="background-color: blue"):
       me.text(text="hi2")
       me.button(label="a button", on_click=click, key="incredibly_long_key")
-      with me.box(background_color="orange"):
+      with me.box(styles="background-color: oragne"):
         me.text(text=f"{state.val} clicks")
