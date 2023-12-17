4695074fe30eed653061b160b5d2806739f2027f
Will Chen <willchen90@gmail.com>
2023-12-17 00:07:45 -0800

    Boost parallelism for playwright to 8

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,8 +19,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  /* Force non-parallelism due to flakiness. */
-  workers: 4,
+  workers: 8,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
