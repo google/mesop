592e63130f20cb5650cc9247f38fceeb7dfd3950
Will Chen <willchen90@gmail.com>
2023-12-18 13:13:50 -0800

    Make text API more ergonomic (text is a positional argument)

--- mesop/components/text/text.py
+++ mesop/components/text/text.py
@@ -6,10 +6,18 @@ from mesop.component_helpers import insert_component
 
 @validate_arguments
 def text(
-  *,
   text: str,
+  *,
   key: str | None = None,
 ):
+  """
+  Create a text component.
+
+  Args:
+      text: The text to display.
+      key: An optional key to uniquely identify the component.
+
+  """
   insert_component(
     key=key, type_name="text", proto=text_pb2.TextType(text=text)
   )
