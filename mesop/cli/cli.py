f108a479be7afd31b946de8242b5a4af8400b87f
Will Chen <willchen90@gmail.com>
2023-12-27 10:43:49 -0800

    Switch logging.log to print for displaying server URL; guarantees it's shown in terminal (and not just a log file)

--- mesop/cli/cli.py
+++ mesop/cli/cli.py
@@ -80,8 +80,7 @@ def main(argv):
     # https://github.com/pallets/werkzeug/blob/eafbed0ce2a6bdf60e62de82bf4a8365188ac334/src/werkzeug/serving.py#L820C9-L820C17
     FgGreen = "\x1b[32m"
     Reset = "\x1b[0m"
-    logging.log(
-      logging.INFO,
+    print(
       f"\n{FgGreen}Running server on: http://localhost:{port()}{Reset}",
     )
     logging.getLogger("werkzeug").setLevel(logging.WARN)
