79eac6793b5392a4313266454a3bc4351c6441a2
Will Chen <willchen90@gmail.com>
2024-01-04 21:42:18 -0800

    Split devtools capabilities into Editor component which wraps shell and is used for dev

---
+++ mesop/web/src/editor/editor.ts
@@ -0,0 +1,134 @@
+import {
+  Component,
+  ElementRef,
+  ErrorHandler,
+  NgZone,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
+import {Router, RouterOutlet, Routes, provideRouter} from '@angular/router';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {
+  ServerError,
+  Component as ComponentProto,
+} from 'mesop/mesop/protos/ui_jspb_proto_pb/mesop/protos/ui_pb';
+import {CommonModule} from '@angular/common';
+import {ComponentRenderer} from '../component_renderer/component_renderer';
+import {ErrorBox} from '../error/error_box';
+import {provideAnimations} from '@angular/platform-browser/animations';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {MatIconModule, MatIconRegistry} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {DevTools} from '../dev_tools/dev_tools';
+import {DevToolsSettings} from '../dev_tools/services/dev_tools_settings';
+import {
+  HotReloadWatcher,
+  IbazelHotReloadWatcher,
+} from '../services/hot_reload_watcher';
+import {GlobalErrorHandlerService} from '../services/global_error_handler';
+import {Shell} from '../shell/shell';
+// Keep the following comment to ensure there's a hook for adding TS imports in the downstream sync.
+// ADD_TS_IMPORT_HERE
+
+@Component({
+  selector: 'optic-editor',
+  templateUrl: 'editor.ng.html',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ComponentRenderer,
+    MatProgressBarModule,
+    ErrorBox,
+    DevTools,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    Shell,
+  ],
+  providers: [
+    {provide: ErrorHandler, useClass: GlobalErrorHandlerService},
+    {provide: HotReloadWatcher, useClass: IbazelHotReloadWatcher},
+  ],
+  styleUrl: 'editor.css',
+})
+class Editor {
+  rootComponent!: ComponentProto;
+
+  @ViewChild('dragHandle', {read: ElementRef}) dragHandle!: ElementRef;
+  private isDragging = false;
+  @ViewChild('sidenav', {read: ElementRef}) sidenav!: ElementRef;
+  @ViewChild('sidenavContent', {read: ElementRef})
+  sidenavContent!: ElementRef;
+  @ViewChild(Shell, {static: false}) shell!: Shell;
+
+  constructor(
+    private zone: NgZone,
+    private renderer: Renderer2,
+    // Injecting to ensure it's loaded in the app
+    private hotReloadWatcher: HotReloadWatcher,
+    // private channel: Channel,
+    private devToolsSettings: DevToolsSettings,
+    iconRegistry: MatIconRegistry,
+    private router: Router,
+    errorHandler: ErrorHandler,
+  ) {
+    iconRegistry.setDefaultFontSetClass('material-symbols-rounded');
+    (errorHandler as GlobalErrorHandlerService).setOnError((error) => {
+      const errorProto = new ServerError();
+      errorProto.setException(`JS Error: ${error.toString()}`);
+      this.errors.push(errorProto);
+    });
+  }
+
+  get errors(): ServerError[] {
+    if (!this.shell) return [];
+    return this.shell.errors;
+  }
+
+  ngAfterViewInit() {
+    this.dragHandle.nativeElement.addEventListener('mousedown', () => {
+      this.isDragging = true;
+    });
+
+    this.renderer.listen(document, 'mousemove', (event) => {
+      if (!this.isDragging) return;
+
+      const newWidth = window.innerWidth - event.clientX;
+      this.sidenav.nativeElement.style.width = `${newWidth}px`;
+      this.sidenavContent.nativeElement.style.marginRight = `${newWidth}px`;
+    });
+
+    this.renderer.listen(document, 'mouseup', (event) => {
+      this.isDragging = false;
+    });
+  }
+
+  showDebugButton() {
+    return this.devToolsSettings.isDebugMode();
+  }
+
+  showDevTools() {
+    return this.devToolsSettings.showDevTools();
+  }
+
+  toggleDevTools() {
+    this.devToolsSettings.toggleShowDevTools();
+  }
+}
+
+const routes: Routes = [{path: '**', component: Editor}];
+
+@Component({
+  selector: 'optic-app',
+  template: '<router-outlet></router-outlet>',
+  standalone: true,
+  imports: [Editor, RouterOutlet],
+})
+class OpticApp {}
+
+export function bootstrapApp() {
+  bootstrapApplication(OpticApp, {
+    providers: [provideAnimations(), provideRouter(routes)],
+  });
+}
--- mesop/web/src/shell/shell.ts
+++ mesop/web/src/shell/shell.ts
@@ -1,12 +1,4 @@
-import {
-  Component,
-  ElementRef,
-  ErrorHandler,
-  HostListener,
-  NgZone,
-  Renderer2,
-  ViewChild,
-} from '@angular/core';
+import {Component, HostListener, NgZone, Renderer2} from '@angular/core';
 import {Router, RouterOutlet, Routes, provideRouter} from '@angular/router';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {
@@ -18,21 +10,9 @@ import {
 import {CommonModule} from '@angular/common';
 import {ComponentRenderer} from '../component_renderer/component_renderer';
 import {Channel, ChannelStatus} from '../services/channel';
-import {ErrorBox} from '../error/error_box';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {bootstrapApplication} from '@angular/platform-browser';
 import {MatIconModule, MatIconRegistry} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {DevTools} from '../dev_tools/dev_tools';
-import {DevToolsSettings} from '../dev_tools/services/dev_tools_settings';
-import {
-  HotReloadWatcher,
-  IbazelHotReloadWatcher,
-} from '../services/hot_reload_watcher';
-import {GlobalErrorHandlerService} from '../services/global_error_handler';
-// Keep the following comment to ensure there's a hook for adding TS imports in the downstream sync.
-// ADD_TS_IMPORT_HERE
 
 @Component({
   selector: 'optic-shell',
@@ -42,44 +22,22 @@ import {GlobalErrorHandlerService} from '../services/global_error_handler';
     CommonModule,
     ComponentRenderer,
     MatProgressBarModule,
-    ErrorBox,
-    DevTools,
     MatIconModule,
-    MatButtonModule,
-    MatSidenavModule,
-  ],
-  providers: [
-    {provide: ErrorHandler, useClass: GlobalErrorHandlerService},
-    {provide: HotReloadWatcher, useClass: IbazelHotReloadWatcher},
   ],
   styleUrl: 'shell.css',
 })
-class Shell {
+export class Shell {
   rootComponent!: ComponentProto;
   errors: ServerError[] = [];
-  @ViewChild('dragHandle', {read: ElementRef}) dragHandle!: ElementRef;
-  private isDragging = false;
-  @ViewChild('sidenav', {read: ElementRef}) sidenav!: ElementRef;
-  @ViewChild('sidenavContent', {read: ElementRef})
-  sidenavContent!: ElementRef;
 
   constructor(
     private zone: NgZone,
     private renderer: Renderer2,
-    // Injecting to ensure it's loaded in the app
-    private hotReloadWatcher: HotReloadWatcher,
     private channel: Channel,
-    private devToolsSettings: DevToolsSettings,
     iconRegistry: MatIconRegistry,
     private router: Router,
-    errorHandler: ErrorHandler,
   ) {
     iconRegistry.setDefaultFontSetClass('material-symbols-rounded');
-    (errorHandler as GlobalErrorHandlerService).setOnError((error) => {
-      const errorProto = new ServerError();
-      errorProto.setException(`JS Error: ${error.toString()}`);
-      this.errors.push(errorProto);
-    });
   }
 
   ngOnInit() {
@@ -97,24 +55,6 @@ class Shell {
     });
   }
 
-  ngAfterViewInit() {
-    this.dragHandle.nativeElement.addEventListener('mousedown', () => {
-      this.isDragging = true;
-    });
-
-    this.renderer.listen(document, 'mousemove', (event) => {
-      if (!this.isDragging) return;
-
-      const newWidth = window.innerWidth - event.clientX;
-      this.sidenav.nativeElement.style.width = `${newWidth}px`;
-      this.sidenavContent.nativeElement.style.marginRight = `${newWidth}px`;
-    });
-
-    this.renderer.listen(document, 'mouseup', (event) => {
-      this.isDragging = false;
-    });
-  }
-
   /** Listen to browser navigation events (go back/forward). */
   @HostListener('window:popstate', ['$event'])
   onPopState(event: Event) {
@@ -125,18 +65,6 @@ class Shell {
 
   isConnectionOpen() {
     return this.channel.getStatus() === ChannelStatus.OPEN;
-  }
-
-  showDebugButton() {
-    return this.devToolsSettings.isDebugMode();
-  }
-
-  showDevTools() {
-    return this.devToolsSettings.showDevTools();
-  }
-
-  toggleDevTools() {
-    this.devToolsSettings.toggleShowDevTools();
   }
 }
 
