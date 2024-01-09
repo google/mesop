faec008c438d163feed57d28655f8020c2569d9a
Will Chen <willchen90@gmail.com>
2024-01-08 17:05:52 -0800

    Create separate testdata subdir for each test case

--- mesop/editor/editor_codemod_test.py
+++ mesop/editor/editor_codemod_test.py
@@ -8,9 +8,9 @@ from mesop.editor.editor_codemod import ReplaceKeywordArg
 from mesop.utils.runfiles import get_runfile_location
 
 
-def load_testdata(filename: str) -> str:
+def load_testdata(dir: str, filename: str) -> str:
   with open(
-    get_runfile_location(f"mesop/mesop/editor/testdata/{filename}")
+    get_runfile_location(f"mesop/mesop/editor/testdata/{dir}/{filename}")
   ) as f:
     return f.read()
 
@@ -22,8 +22,8 @@ class TestReplaceKeywordArg(CodemodTest):
     test_case_name = "simple_callsite"
 
     self.assertCodemod(
-      load_testdata(f"{test_case_name}_before.py"),
-      load_testdata(f"{test_case_name}_after.py"),
+      load_testdata(dir=test_case_name, filename="before.py"),
+      load_testdata(dir=test_case_name, filename="after.py"),
       fn_name="input",
       keyword_arg="label",
       arg_value="defa",
