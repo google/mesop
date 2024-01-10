eb83b159835be350d0b31b05ffcdaf347ba147c3
Will Chen <willchen90@gmail.com>
2024-01-10 10:02:11 -0800

    Deflake divider test by using more explicit selectors that wait for app to load

--- mesop/components/divider/e2e/divider_test.ts
+++ mesop/components/divider/e2e/divider_test.ts
@@ -2,5 +2,6 @@ import {test, expect} from '@playwright/test';
 
 test('test', async ({page}) => {
   await page.goto('/components/divider/e2e/divider_app');
-  expect(await page.textContent('body')).toContain('beforeafter');
+  expect(await page.getByText('before').textContent()).toContain('before');
+  expect(await page.getByText('after').textContent()).toContain('after');
 });
