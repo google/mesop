074af442a35c77d49e36fc70fe9eb298713b1497
Will Chen <willchen90@gmail.com>
2024-01-12 13:21:31 -0800

    Fix equality check for isComponentNameEquals

--- mesop/web/src/utils/proto.ts
+++ mesop/web/src/utils/proto.ts
@@ -1,6 +1,12 @@
 import {ComponentName} from 'mesop/mesop/protos/ui_jspb_proto_pb/mesop/protos/ui_pb';
 
-export function isComponentNameEquals(a: ComponentName, b: ComponentName) {
+export function isComponentNameEquals(
+  a: ComponentName | undefined,
+  b: ComponentName | undefined,
+) {
+  // If either is undefined (even if both is undefined)
+  // we should not treat them as equals.
+  if (a === undefined || b === undefined) return false;
   return (
     a.getFnName() === b.getFnName() &&
     a.getCoreModule() === b.getCoreModule() &&
