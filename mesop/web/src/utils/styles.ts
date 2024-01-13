ca97bc8c2b2075c20f05a27c0aefafeca403f26b
Will Chen <willchen90@gmail.com>
2024-01-13 10:39:59 -0800

    Add new style properties to formatStyles (and alphabetize)

--- mesop/web/src/utils/styles.ts
+++ mesop/web/src/utils/styles.ts
@@ -5,44 +5,12 @@ export function formatStyle(styleObj: Style): string {
   if (!styleObj) {
     return style;
   }
-  if (styleObj.getBackground()) {
-    style += `background: ${styleObj.getBackground()};`;
-  }
-  if (styleObj.getColumns()) {
-    style += `columns: ${styleObj.getColumns()};`;
-  }
-  if (styleObj.getColor()) {
-    style += `color: ${styleObj.getColor()};`;
-  }
-  if (styleObj.getFontWeight()) {
-    style += `font-weight: ${styleObj.getFontWeight()};`;
-  }
-  if (styleObj.getFontSize()) {
-    style += `font-size: ${styleObj.getFontSize()};`;
-  }
-  if (styleObj.getHeight()) {
-    style += `height: ${styleObj.getHeight()};`;
-  }
-  if (styleObj.getWidth()) {
-    style += `width: ${styleObj.getWidth()};`;
-  }
-  if (styleObj.getDisplay()) {
-    style += `display: ${styleObj.getDisplay()};`;
-  }
-  if (styleObj.getFlexDirection()) {
-    style += `flex-direction: ${styleObj.getFlexDirection()};`;
-  }
-  if (styleObj.getFlexGrow()) {
-    style += `flex-grow: ${styleObj.getFlexGrow()};`;
-  }
   if (styleObj.getAlignItems()) {
     style += `align-items: ${styleObj.getAlignItems()};`;
   }
-  if (styleObj.getPosition()) {
-    style += `position: ${styleObj.getPosition()};`;
-  }
-  if (styleObj.getTextAlign()) {
-    style += `text-align: ${styleObj.getTextAlign()};`;
+
+  if (styleObj.getBackground()) {
+    style += `background: ${styleObj.getBackground()};`;
   }
   if (styleObj.getBorder()) {
     const border = styleObj.getBorder()!;
@@ -63,13 +31,79 @@ export function formatStyle(styleObj: Style): string {
       style += `border-right: ${right.getWidth()} ${right.getStyle()} ${right.getColor()};`;
     }
   }
+  if (styleObj.getColor()) {
+    style += `color: ${styleObj.getColor()};`;
+  }
+  if (styleObj.getColumns()) {
+    style += `columns: ${styleObj.getColumns()};`;
+  }
+  if (styleObj.getDisplay()) {
+    style += `display: ${styleObj.getDisplay()};`;
+  }
+  if (styleObj.getFlexBasis()) {
+    style += `flex-basis: ${styleObj.getFlexBasis()};`;
+  }
+  if (styleObj.getFlexDirection()) {
+    style += `flex-direction: ${styleObj.getFlexDirection()};`;
+  }
+  if (styleObj.getFlexGrow()) {
+    style += `flex-grow: ${styleObj.getFlexGrow()};`;
+  }
+  if (styleObj.getFlexShrink()) {
+    style += `flex-shrink: ${styleObj.getFlexShrink()};`;
+  }
+  if (styleObj.getFlexWrap()) {
+    style += `flex-wrap: ${styleObj.getFlexWrap()};`;
+  }
+  if (styleObj.getFontSize()) {
+    style += `font-size: ${styleObj.getFontSize()};`;
+  }
+  if (styleObj.getFontStyle()) {
+    style += `font-style: ${styleObj.getFontStyle()};`;
+  }
+  if (styleObj.getFontWeight()) {
+    style += `font-weight: ${styleObj.getFontWeight()};`;
+  }
+  if (styleObj.getHeight()) {
+    style += `height: ${styleObj.getHeight()};`;
+  }
+  if (styleObj.getJustifyContent()) {
+    style += `justify-content: ${styleObj.getJustifyContent()};`;
+  }
+  if (styleObj.getLetterSpacing()) {
+    style += `letter-spacing: ${styleObj.getLetterSpacing()};`;
+  }
   if (styleObj.getMargin()) {
     const margin = styleObj.getMargin()!;
     style += `margin: ${margin.getTop()} ${margin.getRight()} ${margin.getBottom()} ${margin.getLeft()};`;
   }
+  if (styleObj.getOverflowX()) {
+    style += `overflow-x: ${styleObj.getOverflowX()};`;
+  }
+  if (styleObj.getOverflowY()) {
+    style += `overflow-y: ${styleObj.getOverflowY()};`;
+  }
   if (styleObj.getPadding()) {
     const padding = styleObj.getPadding()!;
     style += `padding: ${padding.getTop()} ${padding.getRight()} ${padding.getBottom()} ${padding.getLeft()};`;
+  }
+  if (styleObj.getPosition()) {
+    style += `position: ${styleObj.getPosition()};`;
+  }
+  if (styleObj.getTextAlign()) {
+    style += `text-align: ${styleObj.getTextAlign()};`;
+  }
+  if (styleObj.getTextDecoration()) {
+    style += `text-decoration: ${styleObj.getTextDecoration()};`;
+  }
+  if (styleObj.getTextOverflow()) {
+    style += `text-overflow: ${styleObj.getTextOverflow()};`;
+  }
+  if (styleObj.getWhiteSpace()) {
+    style += `white-space: ${styleObj.getWhiteSpace()};`;
+  }
+  if (styleObj.getWidth()) {
+    style += `width: ${styleObj.getWidth()};`;
   }
   return style;
 }
