7077e7d58f4fdb81160c8ceddf7120973e8314bb
Rubin Zhang <rczhang@users.noreply.github.com>
2024-02-01 23:01:36 +0000

    Add Linux support for scripts (#38)
    * kill(1) on linux requires pid, fix script exiting
    
    * more fixes for kill(1) on linux
    
    * fix

--- scripts/run_py_dev.sh
+++ scripts/run_py_dev.sh
@@ -1,2 +1,2 @@
-lsof -t -i:32123 | xargs kill && \
+(lsof -t -i:32123 | xargs kill) || true && \
 ibazel run //mesop/cli:dev_cli -- --path="mesop/mesop/example_index.py"
--- scripts/run_web_dev.sh
+++ scripts/run_web_dev.sh
@@ -1,2 +1,2 @@
-lsof -t -i:4200 | xargs kill && \
+(lsof -t -i:4200 | xargs kill) || true && \
 MESOP_SERVER_HOST="http://localhost:32123" ibazel run //mesop/web/src/app/dev:server
