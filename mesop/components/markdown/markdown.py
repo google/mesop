a8d803106d5e44819511ae5009b31724d3e9d40b
Will Chen <willchen90@gmail.com>
2024-02-12 18:03:26 -0800

    Add style argument to markdown component

--- mesop/components/markdown/markdown.py
+++ mesop/components/markdown/markdown.py
@@ -1,11 +1,16 @@
 import mesop.components.markdown.markdown_pb2 as markdown_pb
-from mesop.component_helpers import insert_component, register_native_component
+from mesop.component_helpers import (
+  Style,
+  insert_component,
+  register_native_component,
+)
 
 
 @register_native_component
 def markdown(
   text: str | None = None,
   *,
+  style: Style | None = None,
   key: str | None = None,
 ):
   """
@@ -13,10 +18,12 @@ def markdown(
 
   Args:
       text: **Required.** Markdown text
+      style: Style to apply to component. Follows [HTML Element inline style API](https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/style).
   """
   insert_component(
     key=key,
     type_name="markdown",
+    style=style,
     proto=markdown_pb.MarkdownType(
       text=text,
     ),
