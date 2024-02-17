9781490163aecbca8b398779950e3b4887ec6e7f
Will Chen <willchen90@gmail.com>
2024-02-16 22:51:57 -0800

    Update runfiles to support non-bazel environments (e.g. pip)

--- mesop/utils/runfiles.py
+++ mesop/utils/runfiles.py
@@ -3,4 +3,10 @@ from rules_python.python.runfiles import runfiles  # type: ignore
 
 def get_runfile_location(identifier: str) -> str:
   """Use this wrapper to retrieve a runfile because this util is replaced in downstream sync."""
+
+  # runfiles doesn't exist when running outside of bazel (e.g. pip install)
+  if runfiles.Create() is None:  # type: ignore
+    # TODO: make this less brittle and don't hard code "../", instead walk up
+    # to the "mesop" directory.
+    return "../" + identifier[len("mesop/mesop/") :]
   return runfiles.Create().Rlocation(identifier)  # type: ignore
