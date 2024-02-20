687148d3f6b0b04bb32879ee931d5a6d614308fd
Will Chen <willchen90@gmail.com>
2024-02-20 09:45:34 -0800

    Update run_py_typecheck.sh to also build generator targets

--- scripts/run_py_typecheck.sh
+++ scripts/run_py_typecheck.sh
@@ -1,5 +1,5 @@
-# Build mesop (need to generate protos)
-bazel build //mesop/cli && \
+# Build all top-level mesop targets (need to generate protos)
+bazel build //generator:all &&  bazel build //mesop/cli && \
 # Create venv and activate it
 bazel run //mesop/cli:cli.venv && \
 source .cli.venv/bin/activate && \
