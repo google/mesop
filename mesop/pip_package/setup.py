7fc38d21cd6f5224ec7a9d66fdf2627c079b8f63
Will Chen <willchen90@gmail.com>
2024-02-23 15:11:47 -0800

    Bump version to 0.4.0

--- mesop/pip_package/setup.py
+++ mesop/pip_package/setup.py
@@ -35,7 +35,7 @@ CONSOLE_SCRIPTS = [
 
 setup(
   name="mesop",
-  version="0.3.4",
+  version="0.4.0",
   description="Build UIs in Python",
   long_description=get_readme(),
   url="https://github.com/google/mesop",
