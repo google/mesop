4135b606661894c1373be268c83e4ca96606f6ed
Will Chen <willchen90@gmail.com>
2024-02-23 14:04:48 -0800

    Implement fs watcher to implement server-portion of hot reloading for pip CLI

--- mesop/bin/bin.py
+++ mesop/bin/bin.py
@@ -1,5 +1,8 @@
+import logging
 import os
 import sys
+import threading
+import time
 from typing import Sequence
 
 from absl import app, flags
@@ -7,7 +10,12 @@ from absl import app, flags
 import mesop.protos.ui_pb2 as pb
 from mesop.cli.execute_module import execute_module, get_module_name_from_path
 from mesop.exceptions import format_traceback
-from mesop.runtime import runtime
+from mesop.runtime import (
+  enable_debug_mode,
+  hot_reload_finished,
+  reset_runtime,
+  runtime,
+)
 from mesop.server.wsgi_app import create_app
 
 FLAGS = flags.FLAGS
@@ -39,14 +47,61 @@ $\u001b[35m mesop {argv[1]}\u001b[0m"""
     )
     sys.exit(1)
 
-  create_app(
-    prod_mode=FLAGS.prod, run_block=lambda: execute_main_module(argv[1])
-  ).run()
+  if not FLAGS.prod:
+    enable_debug_mode()
+
+  absolute_path = absolute_path = make_path_absolute(argv[1])
+  app = create_app(
+    prod_mode=FLAGS.prod,
+    run_block=lambda: execute_main_module(absolute_path=absolute_path),
+  )
+
+  # WARNING: this needs to run *after* the initial `execute_module`
+  # has completed, otherwise there's a potential race condition where the
+  # background thread and main thread are running `execute_module` which
+  # leads to obscure hot reloading bugs.
+  if not FLAGS.prod:
+    print("Running with hot reload:")
+    stdin_thread = threading.Thread(
+      target=lambda: fs_watcher(absolute_path), name="mesop_fs_watcher_thread"
+    )
+    stdin_thread.daemon = True
+    stdin_thread.start()
+
+  app.run()
 
 
-def execute_main_module(path_arg: str):
+def fs_watcher(absolute_path: str):
+  """
+  Naive filesystem watcher for the main module of the Mesop application.
+
+  In the future, we can watch for the entire directory, but this captures the main 80% case.
+  """
+  last_modified = os.path.getmtime(absolute_path)
+  while True:
+    # Get the current modification time
+    current_modified = os.path.getmtime(absolute_path)
+
+    # Compare the current modification time with the last modification time
+    if current_modified != last_modified:
+      # Update the last modification time
+      last_modified = current_modified
+      try:
+        print("Hot reload: starting...")
+        reset_runtime()
+        execute_main_module(absolute_path=absolute_path)
+        hot_reload_finished()
+        print("Hot reload: finished!")
+      except Exception as e:
+        logging.log(
+          logging.ERROR, "Could not hot reload due to error:", exc_info=e
+        )
+
+    time.sleep(0.2)
+
+
+def execute_main_module(absolute_path: str):
   try:
-    absolute_path = make_path_absolute(path_arg)
     execute_module(
       module_path=absolute_path,
       module_name=get_module_name_from_path(absolute_path),
