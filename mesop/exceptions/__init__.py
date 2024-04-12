e8fe30d153d94a3a4dc08c096188b1df63f4a53b
Will Chen <willchen90@gmail.com>
2024-04-11 23:08:44 -0700

    Make MesopException the parent class of more specific mesop exceptions & expose as public API

--- mesop/exceptions/__init__.py
+++ mesop/exceptions/__init__.py
@@ -5,16 +5,16 @@ class MesopException(Exception):
   pass
 
 
-class MesopUserException(Exception):
+class MesopUserException(MesopException):
   def __str__(self):
     return f"**User Error:** {super().__str__()}"
 
 
-class MesopInternalException(Exception):
+class MesopInternalException(MesopException):
   def __str__(self):
     return f"**Mesop Internal Error:** {super().__str__()}"
 
 
-class MesopDeveloperException(Exception):
+class MesopDeveloperException(MesopException):
   def __str__(self):
     return f"**Mesop Developer Error:** {super().__str__()}"
