af1c72ad712c7468f3cab9d8bd2110341ffca3b6
Will Chen <willchen90@gmail.com>
2024-04-22 09:27:58 -0700

    Fix setup_proto_py_modules.sh (#130)

--- scripts/setup_proto_py_modules.sh
+++ scripts/setup_proto_py_modules.sh
@@ -2,7 +2,9 @@
 bazel_bin_path=$(bazel info bazel-bin)
 
 touch "${bazel_bin_path}/mesop/__init__.py" && \
-touch "${bazel_bin_path}/generator/__init__.py" && \
 touch "${bazel_bin_path}/mesop/protos/__init__.py" && \
-touch "${bazel_bin_path}/mesop/components/__init__.py" &&
-find "${bazel_bin_path}/mesop/components/" -type d -exec touch {}/__init__.py \;
+touch "${bazel_bin_path}/mesop/components/__init__.py" && \
+find "${bazel_bin_path}/mesop/components/" -type d -exec touch {}/__init__.py \; && \
+# Purposefully have generator at the end since it's not always built and might error out
+# which isn't a big deal because VS Code can still type-check the rest of the codebase.
+touch "${bazel_bin_path}/generator/__init__.py";
