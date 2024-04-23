c91544d9ac12749cbed41b638abc56030ace357e
Will Chen <willchen90@gmail.com>
2024-04-22 21:09:49 -0700

    Plumb style for markdown (#132)

--- mesop/components/markdown/markdown.ts
+++ mesop/components/markdown/markdown.ts
@@ -1,10 +1,12 @@
 import {ChangeDetectorRef, Component, Input} from '@angular/core';
 import {
   Key,
+  Style,
   Type,
 } from 'mesop/mesop/protos/ui_jspb_proto_pb/mesop/protos/ui_pb';
 import {MarkdownType} from 'mesop/mesop/components/markdown/markdown_jspb_proto_pb/mesop/components/markdown/markdown_pb';
 import {marked} from '../../web/external/marked';
+import {formatStyle} from '../../web/src/utils/styles';
 
 @Component({
   selector: 'mesop-markdown',
@@ -14,6 +16,7 @@ import {marked} from '../../web/external/marked';
 export class MarkdownComponent {
   @Input({required: true}) type!: Type;
   @Input() key!: Key;
+  @Input() style!: Style;
   markdownHTML = '';
 
   private _config!: MarkdownType;
@@ -31,5 +34,9 @@ export class MarkdownComponent {
 
   config(): MarkdownType {
     return this._config;
+  }
+
+  getStyle(): string {
+    return formatStyle(this.style);
   }
 }
