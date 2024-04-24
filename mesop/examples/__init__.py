9b14af6bdebf2a75157f1967f0e31137de8ab233
Will Chen <willchen90@gmail.com>
2024-04-23 21:12:14 -0700

    Rename simple{_test} -> many_checkboxes{_test} & deflake by reducing # of checkboxes

--- mesop/examples/__init__.py
+++ mesop/examples/__init__.py
@@ -13,11 +13,11 @@ from mesop.examples import generator as generator
 from mesop.examples import grid as grid
 from mesop.examples import index as index
 from mesop.examples import integrations as integrations
+from mesop.examples import many_checkboxes as many_checkboxes
 from mesop.examples import nested as nested
 from mesop.examples import playground as playground
 from mesop.examples import playground_critic as playground_critic
 from mesop.examples import readme_app as readme_app
-from mesop.examples import simple as simple
 from mesop.examples import sxs as sxs
 from mesop.examples import testing as testing
 from mesop.examples import text_io as text_io
