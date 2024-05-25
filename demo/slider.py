af0a24359a229071e683ec5cdadca9a3f29edafc
Richard <gohn76@gmail.com>
2024-05-24 23:52:53 -0700

    Allow user to set value for slider.
    This change allows the value to be set for the slider. The implemented
    behavior here is similar to the input/textarea value field.
    
    The value will update the value of the slider only if the value being
    passed in changes.
    
    This is for the following use cases:
    
    1. Setting an initial value
    2. Updating the value programmatically. For example if you wanted a
       textbox to control the slider value
    3. Only updating the value if it changes, allows the slider to move
       smoothly (same issue as the text inputs)

--- demo/slider.py
+++ demo/slider.py
@@ -3,14 +3,29 @@ import mesop as me
 
 @me.stateclass
 class State:
-  value: float = 0
-
-
-def on_value_change(event: me.SliderValueChangeEvent):
-  me.state(State).value = event.value
+  initial_input_value: str = "50.0"
+  initial_slider_value: float = 50.0
+  slider_value: float = 50.0
 
 
 @me.page(path="/slider")
 def app():
-  me.slider(on_value_change=on_value_change)
-  me.text(text=f"Value: {me.state(State).value}")
+  state = me.state(State)
+  with me.box(style=me.Style(display="flex", flex_direction="column")):
+    me.input(
+      label="Slider value", value=state.initial_input_value, on_input=on_input
+    )
+    me.slider(on_value_change=on_value_change, value=state.initial_slider_value)
+    me.text(text=f"Value: {me.state(State).slider_value}")
+
+
+def on_value_change(event: me.SliderValueChangeEvent):
+  state = me.state(State)
+  state.slider_value = event.value
+  state.initial_input_value = str(state.slider_value)
+
+
+def on_input(event: me.InputEvent):
+  state = me.state(State)
+  state.initial_slider_value = float(event.value)
+  state.slider_value = state.initial_slider_value
