c9928dc4ffa870273630abe1811d66059b79de5b
Will Chen <willchen90@gmail.com>
2024-05-27 21:55:05 -0700

    Create an editor_cli target to make edit/refresh cycle faster

--- scripts/cli.sh
+++ scripts/cli.sh
@@ -1,2 +1,3 @@
+# Uses editor_cli which provides a faster development cycle than the regular "cli" target.
 (lsof -t -i:32123 | xargs kill) || true && \
-ibazel run //mesop/cli -- --path="mesop/mesop/example_index.py" --reload_demo_modules
+ibazel run //mesop/cli:editor_cli -- --path="mesop/mesop/example_index.py" --reload_demo_modules
