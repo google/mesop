7400eb73af0a83713dcf46d62722a79b1b3b6815
Zaid Bin Shahab <75293628+T-Zaid@users.noreply.github.com>
2024-06-12 08:23:43 +0500

    Implement autoplay option for audio component (#398)

--- demo/audio.py
+++ demo/audio.py
@@ -8,6 +8,14 @@ import mesop as me
   path="/audio",
 )
 def app():
+  """
+  In order to autoplay audio, set the `autoplay` attribute to `True`,
+  Note that there are autoplay restrictions in modern browsers, including Chrome,
+  are designed to prevent audio or video from playing automatically without user interaction.
+  This is intended to improve user experience and reduce unwanted interruptions.
+  You can check the [autoplay ability of your application](https://developer.mozilla.org/en-US/docs/Web/Media/Autoplay_guide#autoplay_availability)
+  """
   me.audio(
     src="https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3",
+    # autoplay=True
   )
