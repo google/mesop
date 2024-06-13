ea4127422f7e6b3f871f48d963f60eae54f5362e
Subhadeep <cloud.subhadeepdas@gmail.com>
2024-06-13 23:16:26 +0530

    Adding e2e tests

---
+++ mesop/components/bokeh_plot/e2e/bokeh_plot_app.py
@@ -0,0 +1,10 @@
+import mesop as me
+
+
+@me.page(path="/components/bokeh_plot/e2e/bokeh_plot_app")
+def app():
+  from bokeh.plotting import figure
+  f = figure()
+  f.title.text = "BOKEH_E2E_FIGURE"
+  # Added bokeh plot with title text BOKEH_E2E_FIGURE
+  me.bokeh_plot(f)
