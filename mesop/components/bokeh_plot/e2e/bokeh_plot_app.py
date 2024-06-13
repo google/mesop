d55a24f996a0f3ef9bc711a2042cc752ba0a7b55
Subhadeep <cloud.subhadeepdas@gmail.com>
2024-06-13 23:17:56 +0530

    fixed e2e lint errors

--- mesop/components/bokeh_plot/e2e/bokeh_plot_app.py
+++ mesop/components/bokeh_plot/e2e/bokeh_plot_app.py
@@ -4,6 +4,7 @@ import mesop as me
 @me.page(path="/components/bokeh_plot/e2e/bokeh_plot_app")
 def app():
   from bokeh.plotting import figure
+
   f = figure()
   f.title.text = "BOKEH_E2E_FIGURE"
   # Added bokeh plot with title text BOKEH_E2E_FIGURE
