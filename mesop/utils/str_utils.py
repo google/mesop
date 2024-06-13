633cd3d739884dcbf3934ddabcbfd2259c56ffd9
Daniel McDonald <101536185+djm93dev@users.noreply.github.com>
2024-06-13 14:48:13 -0400

    Update str_utils.py
    fix formatting

--- mesop/utils/str_utils.py
+++ mesop/utils/str_utils.py
@@ -1,7 +1,8 @@
 import re
 
+
 def snake_case(str: str) -> str:
   """
   Split UpperCamelCase and make it snake_case
   """
-  return re.sub(r'(?<!^)(?=[A-Z])', '_', str).lower()
+  return re.sub(r"(?<!^)(?=[A-Z])", "_", str).lower()
