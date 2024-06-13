c956325f14075398534adea72ee52c033ca59890
Subhadeep <cloud.subhadeepdas@gmail.com>
2024-06-13 23:46:21 +0530

    Fixed e2e page path

--- mesop/components/bokeh_plot/e2e/bokeh_plot_test.ts
+++ mesop/components/bokeh_plot/e2e/bokeh_plot_test.ts
@@ -1,7 +1,7 @@
 import {test, expect} from '@playwright/test';
 
 test('test', async ({page}) => {
-  await page.goto('/components/html/e2e/html_app');
+  await page.goto('/components/bokeh_plot/e2e/bokeh_plot_app');
   // checking if we can found any element with the text BOKEH_E2E_FIGURE
   expect(await page.textContent()).toContain('BOKEH_E2E_FIGURE');
 });
