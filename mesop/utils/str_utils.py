3bac080b83e53edfaf5043fe8e6ff67f7daf9674
Daniel Joseph McDonald <daniel@danielmcdonald.dev>
2024-06-13 14:32:47 -0400

    use re to convert UpperCamelCase to snake_case

--- mesop/utils/str_utils.py
+++ mesop/utils/str_utils.py
@@ -1,10 +1,7 @@
+import re
+
 def snake_case(str: str) -> str:
   """
   Split UpperCamelCase and make it snake_case
   """
-  snake_str = [str[0].lower()]
-  for char in str[1:]:
-    if char.isupper():
-      snake_str.append("_")
-    snake_str.append(char.lower())
-  return "".join(snake_str)
+  return re.sub(r'(?<!^)(?=[A-Z])', '_', str).lower()
