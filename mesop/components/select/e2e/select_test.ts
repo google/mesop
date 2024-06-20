0ff0ccb43a00e0b59ab5cb72ab7e073879b5563b
Richard To <gohn76@gmail.com>
2024-06-19 17:12:34 -0700

    Allow multiple selections on select box (#481)
    * Allow multiple selections on select box
    
    * Fix typing

--- mesop/components/select/e2e/select_test.ts
+++ mesop/components/select/e2e/select_test.ts
@@ -1,11 +1,25 @@
 import {test, expect} from '@playwright/test';
 
-test('test', async ({page}) => {
+test('single selection', async ({page}) => {
   await page.goto('/components/select/e2e/select_app');
-  await page.getByLabel('Select').click();
+  await page.getByRole('combobox').click();
   await page.getByRole('option', {name: 'label 2'}).click();
+  await expect(page.getByText('Selected value: value2')).toBeAttached();
 
-  expect(
-    await page.getByText('Selected value: value2').textContent(),
-  ).toContain('Selected value: value2');
+  await page.getByRole('combobox').click();
+  await page.getByRole('option', {name: 'label 3'}).click();
+  await expect(page.getByText('Selected value: value3')).toBeAttached();
+});
+
+test('multiple selection', async ({page}) => {
+  await page.goto('/components/select/e2e/select_app_multiple');
+  await page.getByLabel('Select').click();
+
+  await page.getByRole('option', {name: 'label 2'}).click();
+  await expect(page.getByText('Selected values: value2')).toBeAttached();
+
+  await page.getByRole('option', {name: 'label 1'}).click();
+  await expect(
+    page.getByText('Selected values: value1, value2'),
+  ).toBeAttached();
 });
