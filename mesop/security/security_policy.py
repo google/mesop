dac1e3aa38a08bb1744eeaa1e9e67e32611053ad
Will Chen <willchen90@gmail.com>
2024-06-24 14:14:42 -0700

    Add SecurityPolicy to docs

--- mesop/security/security_policy.py
+++ mesop/security/security_policy.py
@@ -7,12 +7,12 @@ class SecurityPolicy:
   A class to represent the security policy.
 
   Attributes:
-      allowed_iframe_parents: A list of allowed iframe parents.
-      allowed_connect_srcs: A list of sites you can connect to, see [MDN](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/connect-src).
-      allowed_connect_srcs: A list of sites you load scripts from, see [MDN](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/script-src).
-      dangerously_disable_trusted_types: A flag to disable trusted types.
-        Highly recommended to not disable trusted types because
-        it's an important web security feature!
+    allowed_iframe_parents: A list of allowed iframe parents.
+    allowed_connect_srcs: A list of sites you can connect to, see [MDN](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/connect-src).
+    allowed_connect_srcs: A list of sites you load scripts from, see [MDN](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/script-src).
+    dangerously_disable_trusted_types: A flag to disable trusted types.
+      Highly recommended to not disable trusted types because
+      it's an important web security feature!
   """
 
   allowed_iframe_parents: list[str] = field(default_factory=list)
