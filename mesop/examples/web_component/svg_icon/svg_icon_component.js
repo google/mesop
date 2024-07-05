5185f4dfe06aa602eb0d116d20b980e4f9e81a75
Hussain Chinoy <ghchinoy@google.com>
2024-07-05 11:37:38 -0600

    fix: after running pre-commit locally

--- mesop/examples/web_component/svg_icon/svg_icon_component.js
+++ mesop/examples/web_component/svg_icon/svg_icon_component.js
@@ -1,24 +1,23 @@
 import {
-    LitElement,
-    svg,
-    html,
-  } from 'https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js';
+  LitElement,
+  svg,
+  html,
+} from 'https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js';
 
 export class SVGIconComponent extends LitElement {
-    static properties = {
-      svg: { type: String }
-    };
+  static properties = {
+    svg: {type: String},
+  };
 
-    constructor() {
-      super();
-    }
-
-    render() {
-      const span = document.createElement('span');
-      span.innerHTML = this.svg;
-      return html`${span}`;
-    }
-  
+  constructor() {
+    super();
   }
-  
+
+  render() {
+    const span = document.createElement('span');
+    span.innerHTML = this.svg;
+    return html`${span}`;
+  }
+}
+
 customElements.define('svg-icon', SVGIconComponent);
