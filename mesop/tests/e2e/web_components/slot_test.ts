20ed97b4d701d12031e27f996950be43d538cc61
Will Chen <willchen90@gmail.com>
2024-07-12 21:56:26 -0700

    add retry for slot test

--- mesop/tests/e2e/web_components/slot_test.ts
+++ mesop/tests/e2e/web_components/slot_test.ts
@@ -1,35 +1,39 @@
 import {test, expect, Page} from '@playwright/test';
 
-test('web components - slot', async ({page}) => {
-  await page.goto('http://localhost:32123/web_component/slot/slot_app');
-  // Make sure the page has loaded:
-  expect(page.getByRole('button', {name: 'Decrement'})).toBeVisible();
+test.describe(() => {
+  // All tests in this describe group will get 2 retry attempts.
+  test.describe.configure({retries: 2});
+  test('web components - slot', async ({page}) => {
+    await page.goto('http://localhost:32123/web_component/slot/slot_app');
+    // Make sure the page has loaded:
+    expect(page.getByRole('button', {name: 'Decrement'})).toBeVisible();
 
-  assertValue(10);
-  await page.getByRole('button', {name: 'increment'}).click();
-  assertValue(11);
-  await page.getByRole('button', {name: 'increment'}).click();
-  assertValue(12);
-  await page.getByRole('button', {name: 'Decrement'}).click();
-  assertValue(11);
-  await page.getByRole('button', {name: 'Decrement'}).click();
-  assertValue(10);
+    assertValue(10);
+    await page.getByRole('button', {name: 'increment'}).click();
+    assertValue(11);
+    await page.getByRole('button', {name: 'increment'}).click();
+    assertValue(12);
+    await page.getByRole('button', {name: 'Decrement'}).click();
+    assertValue(11);
+    await page.getByRole('button', {name: 'Decrement'}).click();
+    assertValue(10);
 
-  async function assertValue(value: number) {
-    // Check that the outer component is displaying the right value.
-    expect(
-      await page
-        .locator('div')
-        .filter({hasText: `Value: ${value} increment Start of`})
-        .textContent(),
-    ).toContain(value.toString());
+    async function assertValue(value: number) {
+      // Check that the outer component is displaying the right value.
+      expect(
+        await page
+          .locator('div')
+          .filter({hasText: `Value: ${value} increment Start of`})
+          .textContent(),
+      ).toContain(value.toString());
 
-    // Check that the inner component is displaying the right value.
-    expect(
-      await page
-        .locator('slot-counter-component')
-        .getByText('Value:')
-        .textContent(),
-    ).toContain(value.toString());
-  }
+      // Check that the inner component is displaying the right value.
+      expect(
+        await page
+          .locator('slot-counter-component')
+          .getByText('Value:')
+          .textContent(),
+      ).toContain(value.toString());
+    }
+  });
 });
