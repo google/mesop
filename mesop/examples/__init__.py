b47ea60130b9d082e088e276d317c08ddce08bfc
Will Chen <willchen90@gmail.com>
2024-07-18 16:13:39 -0700

    Create starter kit template (#631)

--- mesop/examples/__init__.py
+++ mesop/examples/__init__.py
@@ -27,6 +27,7 @@ from mesop.examples import playground as playground
 from mesop.examples import playground_critic as playground_critic
 from mesop.examples import readme_app as readme_app
 from mesop.examples import scroll_into_view as scroll_into_view
+from mesop.examples import starter_kit as starter_kit
 from mesop.examples import sxs as sxs
 from mesop.examples import testing as testing
 from mesop.examples import viewport_size as viewport_size
