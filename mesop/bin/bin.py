4480b63c5d3633b1f04b526b24f1e54f470a2c52
Will Chen <willchen90@gmail.com>
2024-07-18 16:25:56 -0700

    Add `mesop init` CLI command (#633)

--- mesop/bin/bin.py
+++ mesop/bin/bin.py
@@ -30,13 +30,45 @@ flags.DEFINE_bool(
 def main(argv: Sequence[str]):
   if len(argv) < 2:
     print(
-      """\u001b[31mERROR: missing command-line argument to Mesop application.\u001b[0m
+      """\u001b[31mERROR: missing command-line argument to Mesop.\u001b[0m
 
-Example command:
+Example run command:
 $\u001b[35m mesop file.py\u001b[0m"""
     )
     sys.exit(1)
 
+  if argv[1] == "init":
+    if len(argv) > 3:
+      print(
+        f"""\u001b[31mERROR: Too many command-line arguments for mesop init.\u001b[0m
+
+Actual:
+$\u001b[35m mesop {" ".join(argv[1:])}\u001b[0m
+
+Re-run with:
+$\u001b[35m mesop init file.py\u001b[0m"""
+      )
+      sys.exit(1)
+
+    filename = argv[2] if len(argv) == 3 else "main.py"
+    absolute_path = make_path_absolute(filename)
+    if os.path.isfile(absolute_path):
+      print(f"\u001b[31mERROR: {filename} already exists.\u001b[0m")
+      sys.exit(1)
+    with open(
+      os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        "../examples/starter_kit/starter_kit.py",
+      )
+    ) as src_file:
+      with open(absolute_path, "w") as dest_file:
+        dest_file.write(src_file.read().replace("/starter_kit", "/"))
+    print(f"""Created starter kit Mesop app at {filename} 🎉
+
+Run Mesop app:
+$\u001b[35m mesop {filename}\u001b[0m
+""")
+    sys.exit(0)
   if len(argv) > 2:
     print(
       f"""\u001b[31mERROR: Too many command-line arguments.\u001b[0m
