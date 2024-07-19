a49e86713d48494af65ef48f9d189a5b5edb3fa9
Richard <gohn76@gmail.com>
2024-07-19 15:09:54 -0700

    Allow data URL for font-src
    In the Code Mirror webcomponent, I noticed that Code Mirror uses data
    URLs for some fonts. This was causing some CSP errors related to fonts.
    
    So adding `data:` to font-src section.

--- mesop/server/static_file_serving.py
+++ mesop/server/static_file_serving.py
@@ -146,7 +146,7 @@ def configure_static_file_serving(
     csp = OrderedDict(
       {
         "default-src": "'self'",
-        "font-src": "fonts.gstatic.com",
+        "font-src": "fonts.gstatic.com data:",
         # Mesop app developers should be able to iframe other sites.
         "frame-src": "*",
         # Mesop app developers should be able to load images and media from various origins.
