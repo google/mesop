c09409024324d25e88dd91cd8199bbddb2efaabb
Richard <gohn76@gmail.com>
2024-07-26 17:01:38 -0700

    Also add mermaidjs support

--- mesop/examples/web_component/markedjs/markedjs_component.js
+++ mesop/examples/web_component/markedjs/markedjs_component.js
@@ -4,6 +4,7 @@ import dompurify from 'https://cdn.jsdelivr.net/npm/dompurify@3.1.6/+esm';
 import {Marked} from 'https://cdn.jsdelivr.net/npm/marked@13.0.2/+esm';
 import {markedHighlight} from 'https://cdn.jsdelivr.net/npm/marked-highlight@2.1.3/+esm';
 import highlightJs from 'https://cdn.jsdelivr.net/npm/highlight.js@11.10.0/+esm';
+import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10.9.1/+esm';
 
 class MarkedJsComponent extends LitElement {
   static properties = {
@@ -12,10 +13,14 @@ class MarkedJsComponent extends LitElement {
 
   constructor() {
     super();
+    mermaid.initialize({startOnLoad: true});
     this.marked = new Marked(
       markedHighlight({
         langPrefix: 'hljs language-',
         highlight(code, lang, info) {
+          if (lang === 'mermaid') {
+            return `<div class="mermaid">${code}</div>`;
+          }
           const language = highlightJs.getLanguage(lang) ? lang : 'plaintext';
           return highlightJs.highlight(code, {language}).value;
         },
