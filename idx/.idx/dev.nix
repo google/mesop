0dd05253c86eb28ce4029f6636c86137f1afddc3
Will Chen <willchen90@gmail.com>
2024-08-01 23:08:13 -0700

    up

---
+++ idx/.idx/dev.nix
@@ -0,0 +1,31 @@
+# No user-configurable parameters
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [ pkgs.python3 ];
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [ "ms-python.python" ];
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        install =
+          "python -m venv .venv && source .venv/bin/activate && pip install -r requirements.txt && mesop init";
+        # Open editors for the following files by default, if they exist:
+        default.openFiles = [ "README.md" "main.py" ];
+      }; # To run something each time the workspace is (re)started, use the `onStart` hook
+    };
+    # Enable previews and customize configuration
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = [ "./devserver.sh" ];
+          env = { PORT = "$PORT"; };
+          manager = "web";
+        };
+      };
+    };
+  };
+}
--- idx/README.md
+++ idx/README.md
@@ -2,3 +2,6 @@
 
 This sets up a custom IDX template for Mesop.
 https://developers.google.com/idx/guides/custom-templates
+
+How to use:
+https://idx.google.com/new?template=https://github.com/wwwillchen/mesop/tree/idx/idx/
