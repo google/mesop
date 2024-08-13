3be695ea0857a18ff6ecda04bb0e92dc426bda5c
Will Chen <willchen90@gmail.com>
2024-08-12 17:13:07 -0700

    attempt 3

--- ai/docbot/deploy_to_hf.sh
+++ ai/docbot/deploy_to_hf.sh
@@ -23,7 +23,7 @@ if [ ! -d "$DEST_PATH" ]; then
 fi
 
 # Build the docs index
-python ai/docbot/docs_index.py --build-index
+cd ai/docbot && python docs_index.py --build-index && cd -
 
 # Get the path of this script which is the demo dir.
 DEMO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
