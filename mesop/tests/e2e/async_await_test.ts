a5a3e800a468e5c00ce173b16a90d4b47a70a103
Jonathan Malmaud <malmaud@gmail.com>
2024-08-15 23:24:38 -0700

    Add e2e test

--- mesop/tests/e2e/async_await_test.ts
+++ mesop/tests/e2e/async_await_test.ts
@@ -2,7 +2,20 @@ import {test, expect} from '@playwright/test';
 
 test('async await', async ({page}) => {
   await page.goto('/async_await');
-  await page.getByRole('button', {name: 'async'}).click();
+  await page.getByRole('button', {name: 'async with yield'}).click();
+  await expect(page.getByText('val1=<async_value>')).toBeVisible({
+    // Intentionally set timeout at 3 seconds to make sure the
+    // async work is happening in parallel.
+    timeout: 3000,
+  });
+  expect(await page.getByText('val2=').textContent()).toEqual(
+    'val2=<async_value>',
+  );
+});
+
+test('async await no yield', async ({page}) => {
+  await page.goto('/async_await');
+  await page.getByRole('button', {name: 'async without yield'}).click();
   await expect(page.getByText('val1=<async_value>')).toBeVisible({
     // Intentionally set timeout at 3 seconds to make sure the
     // async work is happening in parallel.
