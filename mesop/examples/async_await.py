6a65aad6d433c633be3f4a73a2d700c374ff08b4
Jonathan Malmaud <malmaud@gmail.com>
2024-08-20 11:28:38 -0700

    Support non-generating coroutines as event handlers. (#807)

--- mesop/examples/async_await.py
+++ mesop/examples/async_await.py
@@ -8,7 +8,8 @@ def page():
   s = me.state(State)
   me.text("val1=" + s.val1)
   me.text("val2=" + s.val2)
-  me.button("async", on_click=click_async)
+  me.button("async with yield", on_click=click_async_with_yield)
+  me.button("async without yield", on_click=click_async_no_yield)
 
 
 @me.stateclass
@@ -23,7 +24,7 @@ async def fetch_dummy_values():
   return "<async_value>"
 
 
-async def click_async(e: me.ClickEvent):
+async def click_async_with_yield(e: me.ClickEvent):
   val1_task = asyncio.create_task(fetch_dummy_values())
   val2_task = asyncio.create_task(fetch_dummy_values())
 
@@ -31,3 +32,12 @@ async def click_async(e: me.ClickEvent):
     val1_task, val2_task
   )
   yield
+
+
+async def click_async_no_yield(e: me.ClickEvent):
+  val1_task = asyncio.create_task(fetch_dummy_values())
+  val2_task = asyncio.create_task(fetch_dummy_values())
+
+  me.state(State).val1, me.state(State).val2 = await asyncio.gather(
+    val1_task, val2_task
+  )
