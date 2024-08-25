ad9cdd9165a52c1ba9010cfee1b85250d55d2f69
Will Chen <willchen90@gmail.com>
2024-08-25 14:20:01 -0700

    Fix docbot deployment (#839)

--- ai/docbot/deploy_to_hf.sh
+++ ai/docbot/deploy_to_hf.sh
@@ -22,16 +22,13 @@ if [ ! -d "$DEST_PATH" ]; then
     mkdir -p "$DEST_PATH"
 fi
 
-# Build the docs index
-cd ai/docbot && uv run docs_index.py --build-index && cd -
-
 # Get the path of this script which is the demo dir.
 DEMO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cp -R "$DEMO_DIR/" "$DEST_PATH"
 echo "Demo files have been copied to $DEST_PATH"
 
-cd "$DEST_PATH/docbot"
+cd "$DEST_PATH"
 echo "Changed directory to $DEST_PATH"
 
 git init
