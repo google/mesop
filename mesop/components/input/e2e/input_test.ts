c536c0fed5d2dd8618bc8f912ea042b44d260269
Richard <gohn76@gmail.com>
2024-09-07 11:51:48 -0700

    Attempt to fix flaky test

--- mesop/components/input/e2e/input_test.ts
+++ mesop/components/input/e2e/input_test.ts
@@ -58,7 +58,10 @@ test('test textarea shortcuts', async ({page}) => {
 
   await textbox.pressSequentially('hi');
   await page.keyboard.press('Enter');
-  await expect(await page.getByText('Submitted: hi hi')).toBeVisible();
+  // Extend timeout a bit due to flakiness.
+  await expect(await page.getByText('Submitted: hi hi')).toBeVisible({
+    timeout: 10000,
+  });
 
   await page.keyboard.down('Meta');
   await page.keyboard.press('s');
