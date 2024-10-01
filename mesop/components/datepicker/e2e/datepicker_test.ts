2776a3233f4d9fb51f40a800edda62c79b65bbbf
Richard To <gohn76@gmail.com>
2024-10-01 07:02:19 -0700

    Fix broken datepicker test (#1001)
    * Fix broken datepicker test
    
    Update the test so that when the month changes, the test won't break.
    
    In this case, if there is an invalid date, opening the calendar will show the current day.

--- mesop/components/datepicker/e2e/datepicker_test.ts
+++ mesop/components/datepicker/e2e/datepicker_test.ts
@@ -9,14 +9,14 @@ test('date picker', async ({page}) => {
   await page.getByText('Selected date: ').click();
   await expect(page.getByText('Selected date: 2024-09-10')).toBeVisible();
 
-  // Enter invalid date
-  await page.locator('//input').click();
-  await page.locator('//input').fill('13/10/2024');
-  await page.getByText('Selected date: 2024-09-10').click();
-  await expect(page.getByText('Selected date: 2024-09-10')).toBeVisible();
-
   // Pick date from calendar
   await page.getByLabel('Open calendar').click();
   await page.locator('//button/span[text()=" 15 "]').click();
+  await expect(page.getByText('Selected date: 2024-09-15')).toBeVisible();
+
+  // Enter invalid date
+  await page.locator('//input').click();
+  await page.locator('//input').fill('13/10/2024');
+  await page.getByText('Selected date: 2024-09-15').click();
   await expect(page.getByText('Selected date: 2024-09-15')).toBeVisible();
 });
