1f34f5669ba2ab064d91a6e11e1da5848bf92077
Richard To <gohn76@gmail.com>
2024-10-01 21:37:09 -0700

    Update grid table example to use dark mode theming (#1000)
    * Update grid table example to use dark mode theming
    * Drive by: Make date picker test more reliable

--- mesop/components/datepicker/e2e/datepicker_test.ts
+++ mesop/components/datepicker/e2e/datepicker_test.ts
@@ -1,6 +1,6 @@
 import {test, expect} from '@playwright/test';
 
-test('date picker', async ({page}) => {
+test('date picker enter valid date', async ({page}) => {
   await page.goto('/components/datepicker/e2e/datepicker_app');
 
   // Enter valid date
@@ -8,15 +8,20 @@ test('date picker', async ({page}) => {
   await page.locator('//input').fill('9/10/2024');
   await page.getByText('Selected date: ').click();
   await expect(page.getByText('Selected date: 2024-09-10')).toBeVisible();
+});
 
+test('date picker enter invalid date', async ({page}) => {
+  await page.goto('/components/datepicker/e2e/datepicker_app');
+  await page.locator('//input').click();
+  await page.locator('//input').fill('13/10/2024');
+  await page.getByText('Selected date: 2024-10-01').click();
+  await expect(page.getByText('Selected date: 2024-10-01')).toBeVisible();
+});
+
+test('date picker select from calendar', async ({page}) => {
+  await page.goto('/components/datepicker/e2e/datepicker_app');
   // Pick date from calendar
   await page.getByLabel('Open calendar').click();
   await page.locator('//button/span[text()=" 15 "]').click();
-  await expect(page.getByText('Selected date: 2024-09-15')).toBeVisible();
-
-  // Enter invalid date
-  await page.locator('//input').click();
-  await page.locator('//input').fill('13/10/2024');
-  await page.getByText('Selected date: 2024-09-15').click();
-  await expect(page.getByText('Selected date: 2024-09-15')).toBeVisible();
+  await expect(page.getByText('Selected date: 2024-10-15')).toBeVisible();
 });
