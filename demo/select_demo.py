31bf7d60cdf8b7799a9645e30e8602ccd45dbd6f
Richard <gohn76@gmail.com>
2024-10-28 15:37:36 -0700

    Fix bug with single select value not showing.
    Seems like in single select mode, the ngModel is a string rather than
    an array.
    
    So we need to update the code to handle both cases.
    
    Closes #1071

--- demo/select_demo.py
+++ demo/select_demo.py
@@ -5,14 +5,20 @@ import mesop as me
 
 @me.stateclass
 class State:
-  selected_values: list[str] = field(
-    default_factory=lambda: ["value1", "value3"]
+  selected_values_1: list[str] = field(
+    default_factory=lambda: ["value1", "value2"]
   )
+  selected_values_2: str = "value1"
 
 
-def on_selection_change(e: me.SelectSelectionChangeEvent):
+def on_selection_change_1(e: me.SelectSelectionChangeEvent):
   s = me.state(State)
-  s.selected_values = e.values
+  s.selected_values_1 = e.values
+
+
+def on_selection_change_2(e: me.SelectSelectionChangeEvent):
+  s = me.state(State)
+  s.selected_values_2 = e.value
 
 
 def load(e: me.LoadEvent):
@@ -30,16 +36,33 @@ def app():
   state = me.state(State)
   with me.box(style=me.Style(margin=me.Margin.all(15))):
     me.select(
-      label="Select",
+      label="Select multiple",
       options=[
         me.SelectOption(label="label 1", value="value1"),
         me.SelectOption(label="label 2", value="value2"),
         me.SelectOption(label="label 3", value="value3"),
       ],
-      on_selection_change=on_selection_change,
+      on_selection_change=on_selection_change_1,
       style=me.Style(width=500),
       multiple=True,
       appearance="outline",
-      value=state.selected_values,
+      value=state.selected_values_1,
     )
-    me.text(text="Selected values: " + ", ".join(state.selected_values))
+    me.text(
+      text="Selected values (multiple): " + ", ".join(state.selected_values_1)
+    )
+
+    me.select(
+      label="Select single",
+      options=[
+        me.SelectOption(label="label 1", value="value1"),
+        me.SelectOption(label="label 2", value="value2"),
+        me.SelectOption(label="label 3", value="value3"),
+      ],
+      on_selection_change=on_selection_change_2,
+      style=me.Style(width=500, margin=me.Margin(top=40)),
+      multiple=False,
+      appearance="outline",
+      value=state.selected_values_2,
+    )
+    me.text(text="Selected values (single): " + state.selected_values_2)
--- mesop/components/select/e2e/select_test.ts
+++ mesop/components/select/e2e/select_test.ts
@@ -5,10 +5,12 @@ test('single selection', async ({page}) => {
   await page.getByRole('combobox').click();
   await page.getByRole('option', {name: 'label 2'}).click();
   await expect(page.getByText('Selected value: value2')).toBeAttached();
+  await expect(page.getByLabel('label').getByText('label 2')).toBeAttached();
 
   await page.getByRole('combobox').click();
   await page.getByRole('option', {name: 'label 3'}).click();
   await expect(page.getByText('Selected value: value3')).toBeAttached();
+  await expect(page.getByLabel('label').getByText('label 3')).toBeAttached();
 });
 
 test('multiple selection', async ({page}) => {
@@ -19,10 +21,12 @@ test('multiple selection', async ({page}) => {
   await expect(
     page.getByText('Selected values: value1, value2, value3'),
   ).toBeAttached();
+  await expect(page.getByText('label 1, label 2, label 3')).toBeAttached();
 
   await page.getByRole('option', {name: 'label 1'}).click();
 
   await expect(
     page.getByText('Selected values: value2, value3'),
   ).toBeAttached();
+  await expect(page.getByText('label 2, label 3')).toBeAttached();
 });
