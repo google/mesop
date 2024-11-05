8efc807ee870663bb748aee3488d43bf9f6bacbe
Will Chen <willchen90@gmail.com>
2024-11-05 14:46:31 -0800

    Send experiment settings on initial page load (#1093)

--- mesop/web/src/services/experiment_service.ts
+++ mesop/web/src/services/experiment_service.ts
@@ -1,10 +1,35 @@
 import {Injectable} from '@angular/core';
 
+interface ExperimentSettings {
+  readonly websocketsEnabled: boolean;
+  readonly concurrentUpdatesEnabled: boolean;
+  readonly experimentalEditorToolbarEnabled: boolean;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class ExperimentService {
-  concurrentUpdatesEnabled = false;
-  experimentalEditorToolbarEnabled = false;
-  websocketsEnabled = false;
+  private readonly settings: ExperimentSettings;
+
+  constructor() {
+    const windowSettings = (window as any).__MESOP_EXPERIMENTS__;
+    this.settings = windowSettings ?? {
+      websocketsEnabled: false,
+      concurrentUpdatesEnabled: false,
+      experimentalEditorToolbarEnabled: false,
+    };
+  }
+
+  get websocketsEnabled(): boolean {
+    return this.settings.websocketsEnabled;
+  }
+
+  get concurrentUpdatesEnabled(): boolean {
+    return this.settings.concurrentUpdatesEnabled;
+  }
+
+  get experimentalEditorToolbarEnabled(): boolean {
+    return this.settings.experimentalEditorToolbarEnabled;
+  }
 }
